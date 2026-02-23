1146e6ea705e673de87c0877fda6b975f8e5f7a4
Momjax <mboukhatem07@mail.com>
2026-02-23 15:56:00 +0100

    feat: Jalon 14 - Gestion globale des reservations par l'Admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,6 +8,7 @@ use App\Entity\Room;
 use App\Entity\User;
 use App\Repository\ClasseRepository;
 use App\Repository\RoomRepository;
+use App\Repository\ReservationRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -84,6 +85,49 @@ class AdminController extends AbstractController
         return $this->render('admin/rooms/create.html.twig');
     }
 
+    #[Route('/rooms/{id}/edit', name: 'app_admin_room_edit', methods: ['GET', 'POST'])]
+    public function editRoom(Room $room, Request $request, EntityManagerInterface $em): Response
+    {
+        if ($request->isMethod('POST')) {
+            $room->setNomSalle($request->request->get('nom_salle'));
+            $room->setCapacite((int) $request->request->get('capacite'));
+            $room->setDescription($request->request->get('description'));
+
+            // On vide les anciens équipements pour remettre les nouveaux
+            foreach ($room->getEquipements() as $equip) {
+                $em->remove($equip);
+            }
+
+            $equipements = $request->request->get('equipements', '');
+            if (!empty($equipements)) {
+                foreach (explode(',', $equipements) as $nomEquip) {
+                    $nomEquip = trim($nomEquip);
+                    if (!empty($nomEquip)) {
+                        $equip = new Equipement();
+                        $equip->setNomEquipement($nomEquip);
+                        $equip->setRoom($room);
+                        $em->persist($equip);
+                    }
+                }
+            }
+
+            $em->flush();
+            $this->addFlash('success', 'Salle "' . $room->getNomSalle() . '" modifiée !');
+            return $this->redirectToRoute('app_admin_rooms');
+        }
+
+        // On prépare la chaîne des équipements pour le champ input
+        $equipNames = [];
+        foreach ($room->getEquipements() as $e) {
+            $equipNames[] = $e->getNomEquipement();
+        }
+
+        return $this->render('admin/rooms/edit.html.twig', [
+            'room' => $room,
+            'equipNames' => implode(', ', $equipNames)
+        ]);
+    }
+
     #[Route('/rooms/{id}', name: 'app_admin_room_show')]
     public function showRoom(Room $room): Response
     {
@@ -128,6 +172,22 @@ class AdminController extends AbstractController
         }
 
         return $this->render('admin/classes/create.html.twig');
+    }
+
+    #[Route('/classes/{id}/edit', name: 'app_admin_classe_edit', methods: ['GET', 'POST'])]
+    public function editClasse(Classe $classe, Request $request, EntityManagerInterface $em): Response
+    {
+        if ($request->isMethod('POST')) {
+            $classe->setClasseName($request->request->get('classe_name'));
+            $em->flush();
+
+            $this->addFlash('success', 'Classe modifiée avec succès.');
+            return $this->redirectToRoute('app_admin_classes');
+        }
+
+        return $this->render('admin/classes/edit.html.twig', [
+            'classe' => $classe,
+        ]);
     }
 
     #[Route('/classes/{id}', name: 'app_admin_classe_show')]
@@ -209,6 +269,47 @@ class AdminController extends AbstractController
         ]);
     }
 
+    #[Route('/users/{id}/edit', name: 'app_admin_user_edit', methods: ['GET', 'POST'])]
+    public function editUser(
+        User $user,
+        Request $request,
+        EntityManagerInterface $em,
+        ClasseRepository $classeRepository,
+        UserRepository $userRepository
+    ): Response {
+        if ($request->isMethod('POST')) {
+            $email = $request->request->get('email');
+
+            // Vérif si le nouvel email n'est pas déjà pris par UN AUTRE
+            $existing = $userRepository->findOneBy(['email' => $email]);
+            if ($existing && $existing !== $user) {
+                $this->addFlash('error', "Cet email est déjà pris.");
+                return $this->redirectToRoute('app_admin_user_edit', ['id' => $user->getId()]);
+            }
+
+            $user->setEmail($email);
+            $user->setFirstname($request->request->get('firstname'));
+            $user->setLastname($request->request->get('lastname'));
+            $user->setRoles([$request->request->get('role')]);
+
+            $classeId = $request->request->get('classe_id');
+            if ($classeId) {
+                $user->setClasse($classeRepository->find($classeId));
+            } else {
+                $user->setClasse(null);
+            }
+
+            $em->flush();
+            $this->addFlash('success', 'Utilisateur mis à jour.');
+            return $this->redirectToRoute('app_admin_users');
+        }
+
+        return $this->render('admin/users/edit.html.twig', [
+            'user' => $user,
+            'classes' => $classeRepository->findAll(),
+        ]);
+    }
+
     #[Route('/users/{id}/delete', name: 'app_admin_user_delete', methods: ['POST'])]
     public function deleteUser(User $user, EntityManagerInterface $em): Response
     {
@@ -232,5 +333,13 @@ class AdminController extends AbstractController
 
         $this->addFlash('success', 'Mot de passe de "' . $user->getFullName() . '" réinitialisé (nouveau: reset123).');
         return $this->redirectToRoute('app_admin_users');
+    }
+
+    #[Route('/reservations', name: 'app_admin_reservations')]
+    public function manageReservations(ReservationRepository $resRepo): Response
+    {
+        return $this->render('admin/reservations.html.twig', [
+            'reservations' => $resRepo->findBy([], ['reservationStart' => 'DESC']),
+        ]);
     }
 }
