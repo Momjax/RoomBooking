5897feb5d0bceb7e73243a0aafcd30173eb20b42
Momjax <mboukhatem07@mail.com>
2026-02-23 15:56:07 +0100

    feat: Jalon 15 - Page profil et navigation finale

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,25 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // Si l'utilisateur est déjà connecté, rediriger vers le dashboard
         if ($this->getUser()) {
             return $this->redirectToRoute('app_dashboard');
         }
 
-        // Récupérer l'erreur de connexion s'il y en a une
         $error = $authenticationUtils->getLastAuthenticationError();
-
-        // Dernier email saisi par l'utilisateur
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
@@ -32,7 +33,23 @@ class SecurityController extends AbstractController
     #[Route('/logout', name: 'app_logout')]
     public function logout(): void
     {
-        // Ce code ne sera jamais exécuté, Symfony intercepte la route
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/profile', name: 'app_profile', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function profile(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        if ($request->isMethod('POST')) {
+            $newPass = $request->request->get('password');
+            if ($newPass) {
+                /** @var User $user */
+                $user->setPassword($passwordHasher->hashPassword($user, $newPass));
+                $em->flush();
+                $this->addFlash('success', 'Votre mot de passe a été mis à jour.');
+            }
+        }
+        return $this->render('security/profile.html.twig');
     }
 }
